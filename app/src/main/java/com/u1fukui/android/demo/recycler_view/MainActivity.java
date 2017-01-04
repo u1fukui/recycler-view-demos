fc10efacdd73c46bbd3b43f8fcabf8ee778440d0
u1 <u1.kobayashi@gmail.com>
2017-01-04 18:59:38 +0900

    Create project.

---
+++ app/src/main/java/com/u1fukui/android/demo/recycler_view/MainActivity.java
@@ -0,0 +1,13 @@
+package com.u1fukui.android.demo.recycler_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
