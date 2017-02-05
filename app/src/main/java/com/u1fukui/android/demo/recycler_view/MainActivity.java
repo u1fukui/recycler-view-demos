0f80a2bb11e6098b66605840803d7e8bfbf57015
u1 <u1.kobayashi@gmail.com>
2017-02-05 23:35:04 +0900

    Add divider.

--- app/src/main/java/com/u1fukui/android/demo/recycler_view/MainActivity.java
+++ app/src/main/java/com/u1fukui/android/demo/recycler_view/MainActivity.java
@@ -3,6 +3,7 @@ package com.u1fukui.android.demo.recycler_view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerItemClick
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
         adapter = new DemoItemRecyclerAdapter();
         adapter.setListener(this);
